911b758c773c51a446ac7771cc45cbf65a120294
Tyler Kim <tyler.ys.kim@gmail.com>
2017-09-15 19:26:18 -0400

    Started the encoder autonomous.

--- src/main/java/org/ftcTeam/opmodes/test/TazeringAutonomousTest.java
+++ src/main/java/org/ftcTeam/opmodes/test/TazeringAutonomousTest.java
@@ -1,6 +1,11 @@
 package org.ftcTeam.opmodes.test;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.ftcTeam.configurations.Team8702Prod;
 import org.ftcbootstrap.ActiveOpMode;
+import org.ftcbootstrap.components.operations.motors.MotorToEncoder;
+import org.ftcbootstrap.components.utils.MotorDirection;
 
 /**
  * Created by tylerkim on 8/25/17.
@@ -8,21 +13,41 @@ import org.ftcbootstrap.ActiveOpMode;
 
 public class TazeringAutonomousTest extends ActiveOpMode {
 
+    private Team8702Prod robot;
+
+    private MotorToEncoder motorToEncoder;
     int step;
 
     @Override
     protected void onInit() {
 
+    //specify configuration name save
+        robot = Team8702Prod.newConfig(hardwareMap, getTelemetryUtil());
+
+        motorToEncoder = new MotorToEncoder( this, robot.motorR);
+        motorToEncoder.setName("motorR");
+
+        getTelemetryUtil().addData("Init", getClass().getSimpleName() + " initialized.");
+        getTelemetryUtil().sendTelemetry();
     }
 
     @Override
     protected void onStart() throws InterruptedException {
         super.onStart();
         step = 1;
+
     }
 
     @Override
     protected void activeLoop() throws InterruptedException {
 
+    getTelemetryUtil().addData("step: " + step, " Current");
+
+        boolean targetReached = false;
+
+        switch(step) {
+            case 1:
+                targetReached = motorToEncoder.runToTarget(1.0, 1240, MotorDirection.MOTOR_FORWARD, DcMotor.RunMode.RUN_USING_ENCODER);
+        }
     }
 }
