04e581a87e868303cce3bd543100972eb816e08f
Daniel Kim <daniel.sw.kim@gmail.com>
2017-11-12 09:22:36 -0500

    Tuned automode framework

--- src/main/java/org/ftcTeam/opmodes/production/AbstractAutoMode.java
+++ src/main/java/org/ftcTeam/opmodes/production/AbstractAutoMode.java
@@ -21,7 +21,10 @@ abstract class AbstractAutoMode extends ActiveOpMode {
         ELMO_UP,
         DONE
     }
-    //Setting Target Reached to false
+    //Setting Target Reached value.
+    //If it is set to true then State moves to next step
+    //Starting of each step, it will set to false so the the can run until
+    // robot set to true
     boolean targetReached = false;
 
     //States
@@ -29,16 +32,17 @@ abstract class AbstractAutoMode extends ActiveOpMode {
 
     //Declare the MotorToEncoder
     private Team8702Prod robot;
+    //Wheel Controller
+    EncoderBasedOmniWheelController wheelController;
     private MotorToEncoder motorToEncoderFR;
     private MotorToEncoder motorToEncoderFL;
     private MotorToEncoder motorToEncoderBR;
     private MotorToEncoder motorToEncoderBL;
 
-    //Wheel Controller
-    EncoderBasedOmniWheelController wheelController;
-
     JewelColorSensorComponent jewelColorSensorComponent;
     ColorValue jewelColorValue = ColorValue.ZILCH;
+
+    ColorValue panelColor = ColorValue.ZILCH;
 
     @Override
     protected void onInit() {
@@ -61,6 +65,8 @@ abstract class AbstractAutoMode extends ActiveOpMode {
 
     @Override
     protected void onStart() throws InterruptedException {
+        // Read color of panel (Red or Blue)
+        panelColor = getPanelColor();
         super.onStart();
     }
 
@@ -68,73 +74,51 @@ abstract class AbstractAutoMode extends ActiveOpMode {
     protected void activeLoop() throws InterruptedException {
 
         switch (currentState) {
-
             case INIT: //Set everything
-
-                //Show Telemetry
-                getTelemetryUtil().addData("Stage", currentState.toString());
-                getTelemetryUtil().sendTelemetry();
-
+                logStage();
                 //Set targetReached to true
                 startTheRobot();
 
                 //Condition if targetReached is true
                 if (targetReached) {
                     currentState = State.ELMO_DOWN;
+                    targetReached = false;
                 }
-                targetReached = false;
                 break;
 
             case ELMO_DOWN: //Bring elmo down
                 //Set 1 second delay
-                sleep(1000);
-
-                //set targetReached to false
-                targetReached = false;
-
-                //Show Telemetry
-                getTelemetryUtil().addData("Stage", currentState.toString());
-                getTelemetryUtil().sendTelemetry();
-
+                logStage();
                 //targetReached is true
                 startTheRobot();
 
                 //If task is complete move to next state
                 if(targetReached) {
                     currentState = State.READ_JEWEL_COLOR;
+                    targetReached = false;
+                    sleep(1000);
                 }
 
                 break;
 
             case READ_JEWEL_COLOR: //Read jewel color
-                sleep(1000);
-                targetReached = false;
                 jewelColorValue = jewelColorSensorComponent.getColor();
 
-                if (jewelColorValue != ColorValue.ZILCH) {
-                    getTelemetryUtil().addData("Jewel Color:", jewelColorValue.toString());
+                getTelemetryUtil().addData("Jewel Color:", jewelColorValue.toString());
+                getTelemetryUtil().sendTelemetry();
+                if (jewelColorValue == ColorValue.RED || jewelColorValue == ColorValue.BLUE) {
                     targetReached = true;
                 }
-                //Show Telemetry
-                getTelemetryUtil().addData("Stage", currentState.toString());
-                getTelemetryUtil().sendTelemetry();
 
                 if(targetReached) {
                     currentState = State.KNOCK_OFF_JEWEL;
+                    targetReached = false;
+                    sleep(1000);
                 }
 
                 break;
             case KNOCK_OFF_JEWEL: //Move robot to appropriate direction for color
-
-                //Delay for 1 second
-                sleep(1000);
-
-                //Set targetReached to false
-                targetReached = false;
-
-                //Show Telemetry
-                getTelemetryUtil().addData("Stage: ", currentState.toString());
-                getTelemetryUtil().sendTelemetry();
+                logStage();
 
                 //move one wheel forward
                 targetReached = motorToEncoderFL.runToTarget(1.0, 1240, MotorDirection.MOTOR_FORWARD, DcMotor.RunMode.RUN_USING_ENCODER);
@@ -142,45 +126,32 @@ abstract class AbstractAutoMode extends ActiveOpMode {
                 //If task is complete move to next state
                 if (targetReached) {
                     currentState = State.ELMO_UP;
+                    targetReached = false;
+                    sleep(1000);
                 }
 
                 break;
 
             case ELMO_UP: //Bring elmo up
-                //Set 1 second delay
-                sleep(1000);
-
-                //Set targetReached to false
-                targetReached = false;
-
-                //Show Telemetry
-                getTelemetryUtil().addData("Stage", currentState.toString());
-                getTelemetryUtil().sendTelemetry();
-
+                logStage();
                 //Set targetReached to true
                 startTheRobot();
 
                 //If task is complete move to next stage
                 if (targetReached) {
                     currentState = State.DONE;
+                    targetReached = false;
+                    sleep(1000);
                 }
 
                 break;
 
             case DONE: //Complete autonomous
                 //Set 1 second delay
-                sleep(1000);
+                logStage();
 
                 //Set targetReached to false
                 targetReached = false;
-
-                //Show Telemetry
-                getTelemetryUtil().addData("Stage", currentState.toString());
-                getTelemetryUtil().sendTelemetry();
-
-                //Set targetReached to true
-                startTheRobot();
-
                 setOperationsCompleted();
                 break;
         }
@@ -188,9 +159,13 @@ abstract class AbstractAutoMode extends ActiveOpMode {
     }
 
     private void startTheRobot() {
-
         // TODO logic later
         targetReached = true;
+    }
+
+    private void logStage() {
+        getTelemetryUtil().addData("Stage", currentState.toString());
+        getTelemetryUtil().sendTelemetry();
     }
     abstract ColorValue getPanelColor();
 }
