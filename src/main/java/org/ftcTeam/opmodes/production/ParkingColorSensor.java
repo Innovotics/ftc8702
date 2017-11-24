651c6091cb70ac73515a42c5bc1e54abd1cb414c
Tyler Kim <tyler.ys.kim@gmail.com>
2017-11-24 17:58:44 -0500

    Parking Color Sensor initial program completed

--- src/main/java/org/ftcTeam/opmodes/production/ParkingColorSensor.java
+++ src/main/java/org/ftcTeam/opmodes/production/ParkingColorSensor.java
@@ -3,6 +3,7 @@ package org.ftcTeam.opmodes.production;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
 import org.ftcTeam.utils.ColorValue;
+import org.ftcbootstrap.components.ColorSensorComponent;
 
 /**
  * Created by dkim on 11/24/17.
@@ -10,14 +11,43 @@ import org.ftcTeam.utils.ColorValue;
 
 public class ParkingColorSensor {
 
-    private ColorSensor colorSensor;
+    //Declare Color Sensor
+    private ColorSensor parkingColorSensor;
+    private ColorSensorComponent colorSensorComponent;
 
+    //Set color Vluae
     public ParkingColorSensor(ColorSensor colorSensor) {
-        this.colorSensor = colorSensor;
+
+        this.parkingColorSensor = colorSensor;
     }
+
 
     public boolean parkingOn(ColorValue baseColor) {
 
-        return true;
+        ColorValue detectingColor = ColorValue.ZILCH;
+        //Determines Color
+        int Red = parkingColorSensor.red();
+        int Blue = parkingColorSensor.blue();
+        int Green = parkingColorSensor.green();
+
+        //Boolean Values
+        boolean redBoolean = colorSensorComponent.isRed(Red, Blue, Green);
+        boolean blueBoolean = colorSensorComponent.isBlue(Red, Blue, Green);
+
+        //Determine which is color to call
+        if (parkingColorSensor.red() > parkingColorSensor.blue() && parkingColorSensor.red() > parkingColorSensor.green()) {
+            detectingColor = ColorValue.RED;
+        }
+
+        if (parkingColorSensor.blue() > parkingColorSensor.red() && parkingColorSensor.green() > parkingColorSensor.red()) {
+            detectingColor = ColorValue.BLUE;
+        }
+
+        //Detect if base color is congruent to detected color
+        if (detectingColor.equals(baseColor)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
