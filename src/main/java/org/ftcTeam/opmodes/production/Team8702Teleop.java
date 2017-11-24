389f1f7c4a7f0da4770ddfbe339a1636b1ad229e
tanyamalik2000 <tanyamalik2000@gmail.com>
2017-11-24 17:57:06 -0500

    Added Touch Sensor Functionality For Teleop

--- src/main/java/org/ftcTeam/opmodes/production/Team8702Teleop.java
+++ src/main/java/org/ftcTeam/opmodes/production/Team8702Teleop.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 import org.ftc8702.opmodes.GamePadOmniWheelDrive;
 import org.ftcTeam.configurations.Team8702Prod;
+import org.ftcTeam.opmodes.test.SensorDigitalTouch;
 import org.ftcTeam.utils.GamePadDuelServo;
 import org.ftcbootstrap.ActiveOpMode;
 import org.ftcbootstrap.components.operations.motors.GamePadMotor;
@@ -17,6 +18,8 @@ public class Team8702Teleop extends ActiveOpMode {
     private GamePadOmniWheelDrive gamePadOmniWheelDrive;
     private GamePadDuelServo clapperGamePadServo;
     private GamePadMotor clapperGamePadMotor;
+    private DigitalChannel topTouchClapperSensor;
+    private DigitalChannel bottomTouchClapperSensor;
 
 
     /**
@@ -42,7 +45,6 @@ public class Team8702Teleop extends ActiveOpMode {
         gamePadOmniWheelDrive.startRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         clapperGamePadServo = new GamePadDuelServo(this, gamepad2, robot.clapperRight, robot.clapperLeft, GamePadDuelServo.Control.X_B, 0.35);
         clapperGamePadMotor = new GamePadMotor(this, gamepad2, robot.clapperMotor, GamePadMotor.Control.UP_DOWN_BUTTONS, 0.1f);
-
     }
 
     /**
@@ -57,6 +59,41 @@ public class Team8702Teleop extends ActiveOpMode {
         clapperGamePadServo.update();
         clapperGamePadMotor.update();
         //getTelemetryUtil().sendTelemetry();
+        isClapperTouchSensorOn();
     }
+    private boolean isClapperTouchSensorOn()
+    {
+        while (opModeIsActive()) {
+
+            // send the info back to driver station using telemetry function.
+            // if the digital channel returns true it's HIGH and the button is unpressed.
+            if (topTouchClapperSensor.getState() == true) {
+                telemetry.addData("Top Touch Clapper Sensor", "Is Pressed");
+                if (gamepad2.y)
+                {
+                    clapperGamePadMotor = new GamePadMotor(this, gamepad2, robot.clapperMotor, GamePadMotor.Control.UP_DOWN_BUTTONS, 0);
+                }
+                else
+                {
+                    clapperGamePadMotor = new GamePadMotor(this, gamepad2, robot.clapperMotor, GamePadMotor.Control.UP_DOWN_BUTTONS, 0.1f);
+                }
+            }
+            else if (bottomTouchClapperSensor.getState() == true){
+                telemetry.addData("Bottom Touch Clapper Sensor", "Is Pressed");
+                if (gamepad2.a)
+                {
+                    clapperGamePadMotor = new GamePadMotor(this, gamepad2, robot.clapperMotor, GamePadMotor.Control.UP_DOWN_BUTTONS, 0);
+                }
+                else
+                {
+                    clapperGamePadMotor = new GamePadMotor(this, gamepad2, robot.clapperMotor, GamePadMotor.Control.UP_DOWN_BUTTONS, 0.1f);
+                }
+            }
+
+            telemetry.update();
+        }
+        return true;
+    }
+
 
 }
