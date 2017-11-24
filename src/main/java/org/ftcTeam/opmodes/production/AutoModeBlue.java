a8b27728ad0f7171cb880513251f321922aaca50
Daniel Kim <daniel.sw.kim@gmail.com>
2017-11-24 16:53:58 -0500

    Enable AutoModeBlue

--- src/main/java/org/ftcTeam/opmodes/production/AutoModeBlue.java
+++ src/main/java/org/ftcTeam/opmodes/production/AutoModeBlue.java
@@ -1,30 +1,15 @@
-//package org.ftcTeam.opmodes.production;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//
-//import org.ftcTeam.utils.ColorValue;
-//
-//
-//@Autonomous(name = "AutoModeBlue", group = "Ops")
-//public class AutoModeBlue extends AbstractAutoMode {
-//
-//    @Override
-//    protected void onInit() {
-//    super.onInit();
-//    }
-//
-//    @Override
-//    protected void onStart() throws InterruptedException {
-//        super.onStart();
-//    }
-//
-//    @Override
-//    protected void activeLoop() throws InterruptedException {
-//        super.activeLoop();
-//    }
-//
-//    protected ColorValue getPanelColor() {
-//        return ColorValue.BLUE;
-//    }
-//
-//}
+package org.ftcTeam.opmodes.production;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.ftcTeam.utils.ColorValue;
+
+
+@Autonomous(name = "AutoModeBlue", group = "Ops")
+public class AutoModeBlue extends AbstractAutoMode {
+
+    protected ColorValue getPanelColor() {
+        return ColorValue.BLUE;
+    }
+
+}
