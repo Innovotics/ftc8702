e00a901548a37b760c7e579bc2e5d0546e7c7192
Tyler Kim <tyler.ys.kim@gmail.com>
2017-12-09 18:39:41 -0500

    Vuforia Prototype

--- src/main/java/org/ftcTeam/opmodes/production/AbstractAutoMode.java
+++ src/main/java/org/ftcTeam/opmodes/production/AbstractAutoMode.java
@@ -1,20 +1,35 @@
 package org.ftcTeam.opmodes.production;
 
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.ftcTeam.configurations.production.Team8702ProdAuto;
 import org.ftcTeam.configurations.production.Team8702RobotConfig;
+import org.ftcTeam.utils.CryptoBoxLocation;
 import org.ftcbootstrap.ActiveOpMode;
 import org.ftcbootstrap.components.ColorSensorComponent;
 import org.ftcTeam.utils.ColorValue;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+
 
 abstract class AbstractAutoMode extends ActiveOpMode {
 
+    //States for actual autonomous
     protected enum State {
         INIT,
-        VUFORIA_DETECTION,
         ELMO_DOWN,
         READ_JEWEL_COLOR,
         KNOCK_OFF_JEWEL,
         ELMO_UP,
+        VUFORIA_DETECTION,
         PARKING,
         DONE
     }
@@ -30,15 +45,25 @@ abstract class AbstractAutoMode extends ActiveOpMode {
 
     //Declare the MotorToEncoder
     private Team8702ProdAuto robot;
+
     //Wheel Controller
     EncoderBasedOmniWheelController wheelController;
 
-
-
+    //Set ColorValue to zilch
     ColorValue jewelColorValue = ColorValue.ZILCH;
     ColorValue panelColor = ColorValue.ZILCH;
+
+    //Declared colorSensorComponent
     public ColorSensorComponent colorSensorComponent;
 
+    //Set cryptoBoxLocation to Unknown
+    private RelicRecoveryVuMark cryptoBoxLocation = RelicRecoveryVuMark.UNKNOWN;
+
+    //Adding Vuforia
+    VuforiaLocalizer vuforia;
+    VuforiaTrackable relicTemplate = null;
+
+    //Set abstract ColorValue
     abstract ColorValue getPanelColor();
     abstract boolean park() throws InterruptedException;
 
@@ -52,12 +77,13 @@ abstract class AbstractAutoMode extends ActiveOpMode {
         //Set state to Init
         currentState = State.INIT;
 
-
         this.elmoOperation = new ElmoOperation(this);
-
 
         //Color Sensor
         colorSensorComponent = new ColorSensorComponent(this, robot.elmoColorSensor, ColorSensorComponent.ColorSensorDevice.MODERN_ROBOTICS_I2C);
+
+        //Init vuforia
+        initVuforia();
 
         getTelemetryUtil().addData("Init", getClass().getSimpleName() + " initialized.");
         getTelemetryUtil().sendTelemetry();
@@ -76,13 +102,17 @@ abstract class AbstractAutoMode extends ActiveOpMode {
         switch (currentState) {
             case INIT: //Set everything
                 logStage();
-                startTheRobot();
+
+            //set targetReached to true
+            startTheRobot();
+
+                //test if targetReached is true
                 if (targetReached) {
                     if (Team8702RobotConfig.ELMO_ON) {
-                        currentState = State.PARKING;
+                        currentState = State.VUFORIA_DETECTION;
                     }
                     else {
-                        currentState = State.PARKING;
+                        currentState = State.VUFORIA_DETECTION;
                     }
                     targetReached = false;
                 }
@@ -124,6 +154,29 @@ abstract class AbstractAutoMode extends ActiveOpMode {
                 }
                 break;
 
+            case VUFORIA_DETECTION: //Detect vuforia
+                logStage();
+
+                //Read crypto message
+                readCryptoMessage();
+
+                //Test if cryptoBox is not UNKNOWN
+                if(cryptoBoxLocation != RelicRecoveryVuMark.UNKNOWN) {
+                    targetReached = true;
+                    getTelemetryUtil().addData("VuMark Location: ", cryptoBoxLocation.toString());
+                    getTelemetryUtil().sendTelemetry();
+                }
+
+                //test if targetReached is true
+                if(targetReached = true) {
+                    //parks
+                    currentState = State.DONE;
+
+                    //resets targetReached
+                    targetReached = false;
+                }
+
+                break;
             case PARKING:
                 logStage();
                 if(!Team8702RobotConfig.AUTO_PARKING_ON) {
@@ -195,4 +248,69 @@ abstract class AbstractAutoMode extends ActiveOpMode {
         getTelemetryUtil().sendTelemetry();
     }
 
+    //Method that Initializes vuforia
+    private void initVuforia() {
+
+        //Sets cameria monitor
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        //Set license
+        parameters.vuforiaLicenseKey = "ARCdiMD/////AAAAGZaUI5b2cEcKvP272hyUYZsbGU1BKSfR6eaOxeU3TcCop8KqmtC2LczpH4/9gwSE38zJYRRFXQNVIyvvMZXjMe93lQiLHCPya9eBokUe/Vwl2T6La6sUwCGxINckaFGwODclWrvSP92ZdmU6Kz5+JN44Jutplpv3QszrWk8OA92L9zkce3nF1ZPkhB0yEQdwkY2nxqwXrCtUYBYSsNhemo/2NZibO/ui6NnvA1Pm90QkxNfg5Zi8MBNK0pxjV2O58pQoCDtmEqYJn1d1yxFxBuhXptpVLLUIH7CT8/nsJw1r2a6YtmQFiIkJOI8TOK1wbHCFqgtFZH97VYI9xSIpeezOZdQZz1P5kAiGsuSAlX5h";
+
+        //Set camera direction and class factory
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        //Set relic tracker
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+
+        //activate relicTrackable
+        relicTrackables.activate();
+
+    }
+
+    //Format method
+    String format(OpenGLMatrix transformationMatrix) {
+        return (transformationMatrix != null) ? transformationMatrix.formatAsTransform() : "null";
+    }
+
+    //Read crypto box
+    private void readCryptoMessage() {
+
+
+        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
+            telemetry.addData("VuMark", "%s visible", vuMark);
+
+            cryptoBoxLocation = vuMark;
+
+            OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
+            telemetry.addData("Pose", format(pose));
+
+            if (pose != null) {
+                VectorF trans = pose.getTranslation();
+                Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+
+                // Extract the X, Y, and Z components of the offset of the target relative to the robot
+                double tX = trans.get(0);
+                double tY = trans.get(1);
+                double tZ = trans.get(2);
+
+                // Extract the rotational components of the target relative to the robot
+                double rX = rot.firstAngle;
+                double rY = rot.secondAngle;
+                double rZ = rot.thirdAngle;
+            }
+        }
+        else {
+            telemetry.addData("VuMark", "not visible");
+        }
+
+        telemetry.update();
+    }
+
 }
+
