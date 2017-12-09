5aa307586e892b075bc89fb62aec38e9c2089d7f
Tyler Kim <tyler.ys.kim@gmail.com>
2017-12-08 21:08:18 -0500

    parking complete

--- src/main/java/org/ftcTeam/opmodes/production/AutoModeRedAngle.java
+++ src/main/java/org/ftcTeam/opmodes/production/AutoModeRedAngle.java
@@ -2,16 +2,40 @@ package org.ftcTeam.opmodes.production;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import org.ftcTeam.utils.ColorValue;
-
 
 @Autonomous(name = "RedAngle", group = "Ops")
-public class AutoModeRedAngle extends AutoModeRed{
+public class AutoModeRedAngle extends AutoModeRed {
 
     @Override
-    boolean park() {
+    boolean park() throws InterruptedException {
+        //Move robot out of the platform
+        getRobot().motorFL.setPower(.5 * (-1));
+        getRobot().motorFR.setPower(.5 * (-1));
+        getRobot().motorBL.setPower(.5 * (1));
+        getRobot().motorBR.setPower(.5 * (1));
+        Thread.sleep(2000);
+
+        //Stop the robot
+        getRobot().motorFL.setPower(.5 * (0));
+        getRobot().motorFR.setPower(.5 * (0));
+        getRobot().motorBL.setPower(.5 * (0));
+        getRobot().motorBR.setPower(.5 * (0));
+        Thread.sleep(1000);
+
+        //Move the robot forwards
+        getRobot().motorFL.setPower(.25 * (-1));
+        getRobot().motorFR.setPower(.25 * (1));
+        getRobot().motorBL.setPower(.25 * (-1));
+        getRobot().motorBR.setPower(.25 * (1));
+        Thread.sleep(1000);
+
+        //Stop the robot
+        getRobot().motorFL.setPower(.5 * (0));
+        getRobot().motorFR.setPower(.5 * (0));
+        getRobot().motorBL.setPower(.5 * (0));
+        getRobot().motorBR.setPower(.5 * (0));
+        Thread.sleep(1000);
+
         return true;
-
     }
-
 }
