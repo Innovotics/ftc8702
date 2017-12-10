c364090e3ed7d811793f2d0fbb88886f5fa96b0a
Daniel Kim <daniel.sw.kim@gmail.com>
2017-12-10 17:07:57 -0500

    E

---
+++ src/main/java/org/ftcTeam/opmodes/test/SensorMRRangeSensor.java
@@ -0,0 +1,52 @@
+package org.ftcTeam.opmodes.test;
+/*
+Modern Robotics Range Sensor Example
+Created 9/8/2016 by Colton Mehlhoff of Modern Robotics using FTC SDK 2.x Beta
+Reuse permitted with credit where credit is due
+
+Configuration:
+I2cDevice on an Interface Module named "range" at the default address of 0x28 (0x14 7-bit)
+
+This program can be run without a battery and Power Destitution Module.
+
+For more information, visit modernroboticsedu.com.
+Support is available by emailing support@modernroboticsinc.com.
+*/
+
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.I2cDevice;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous(name = "Sensor: MR range sensor", group = "Sensor")
+public class SensorMRRangeSensor extends LinearOpMode {
+
+    ModernRoboticsI2cRangeSensor rangeSensor;
+
+    @Override public void runOpMode() {
+
+        // get a reference to our compass
+        rangeSensor = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "sensor_range");
+
+        // wait for the start button to be pressed
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("raw ultrasonic", rangeSensor.rawUltrasonic());
+            telemetry.addData("raw optical", rangeSensor.rawOptical());
+            telemetry.addData("cm optical", "%.2f cm", rangeSensor.cmOptical());
+            telemetry.addData("cm", "%.2f cm", rangeSensor.getDistance(DistanceUnit.CM));
+            telemetry.update();
+        }
+    }
+
+}
