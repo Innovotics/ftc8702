ec5b4cb01fd5cfa6d1b631802f34324b76d48351
Ben Szeto <benkszeto@gmail.com>
2018-10-08 15:39:29 -0400

    Color Sensor:

--- src/main/java/org/ftc8702/utils/RobotProperties.java
+++ src/main/java/org/ftc8702/utils/RobotProperties.java
@@ -9,4 +9,8 @@ public class RobotProperties {
     //Motor
     public static final String MOTOR_LEFT = "motorL";
     public static final String MOTOR_RIGHT= "motorR";
+
+    //Color sensor
+    public static final String COLOR_SENSOR_FRONT_LEFT= "colorSensorFL";
+    public static final String COLOR_SENSOR_FRONT_RIGHT= "colorSensorFR";
 }
