efb803c5d8fbc54bf5ccfe9f16eac6fb8a289b5e
nongooman <daniel.sw.kim@gmail.com>
2018-10-20 15:38:58 -0400

    Refactoring :

--- src/main/java/org/ftc8702/components/motors/FourWheelMotors.java
+++ src/main/java/org/ftc8702/components/motors/FourWheelMotors.java
@@ -1,4 +1,4 @@
-package org.ftc8702.opmodes;
+package org.ftc8702.components.motors;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 
---
+++ src/main/java/org/ftc8702/opmodes/production/AutoModePrimary.java
@@ -0,0 +1,21 @@
+package org.ftc8702.opmodes.production;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+import org.ftcTeam.utils.ColorValue;
+
+
+@Autonomous(name = "AutoModePrimary", group = "Ops")
+@Disabled
+public class AutoModePrimary extends AbstractAutoMode {
+    @Override
+    ColorValue getPanelColor() {
+        return null;
+    }
+
+    @Override
+    boolean park() throws InterruptedException {
+        return false;
+    }
+}
