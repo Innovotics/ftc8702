49815e5e199408845c1842cfdb53f8a6c57cd970
Jeff Szeto <szetojeff@gmail.com>
2019-11-19 22:20:10 -0500

    resolved conflict and added ServoTest

--- src/main/java/org/ftc8702/configurations/production/SkystoneAutoConfig.java
+++ src/main/java/org/ftc8702/configurations/production/SkystoneAutoConfig.java
@@ -2,6 +2,7 @@ package org.ftc8702.configurations.production;
 
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.ftc8702.components.motors.MecanumWheelDriveTrain;
 import org.ftc8702.opmodes.production.SkystoneFlexArm;
@@ -10,7 +11,6 @@ import org.ftc8702.opmodes.production.SkystoneJaJa;
 import org.ftc8702.opmodes.production.SkystoneSlideAndBrickPicker;
 
 import ftcbootstrap.components.utils.TelemetryUtil;
-import com.qualcomm.robotcore.hardware.Servo;
 
 public class SkystoneAutoConfig extends AbstractRobotConfiguration {
 
@@ -32,20 +32,12 @@ public class SkystoneAutoConfig extends AbstractRobotConfiguration {
 
         ProdMecanumRobotConfiguration mecanumConfig = ProdMecanumRobotConfiguration.newConfig(hardwareMap, telemetryUtil);
         driveTrain = new MecanumWheelDriveTrain(mecanumConfig.motorFL,mecanumConfig.motorFR,mecanumConfig.motorBL,mecanumConfig.motorBR);
-<<<<<<< HEAD
-
-=======
->>>>>>> d770b71bcaf7cbbb895b80ab59f2fdcc87e95f35
         jaja = new SkystoneJaJa(hardwareMap.get(Servo.class, "foundationGrabberL"), hardwareMap.get(Servo.class, "foundationGrabberR"));
         FlexArm = new SkystoneFlexArm(mecanumConfig.SliderArmLeft, mecanumConfig.SliderArmRight);
         Slider = new SkystoneSlideAndBrickPicker(hardwareMap.get(Servo.class, "brickPicker"),(hardwareMap.get(Servo.class, "linearSlide")));
         Intake = new SkystoneIntake(mecanumConfig.IntakeWheelLeft, mecanumConfig.IntakeWheelRight);
         colorSensor = hardwareMap.get(ColorSensor.class, "colorSensor");
-<<<<<<< HEAD
-
-=======
         telemetryUtil.addData("Color Sensor", colorSensor+"");
->>>>>>> d770b71bcaf7cbbb895b80ab59f2fdcc87e95f35
         telemetryUtil.sendTelemetry();
     }
 }
---
+++ src/main/java/org/ftc8702/opmodes/test/ServoTest.java
@@ -0,0 +1,56 @@
+package org.ftc8702.opmodes.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+import ftcbootstrap.ActiveOpMode;
+
+@TeleOp(name = "ServoTest", group = "production")
+public class ServoTest extends ActiveOpMode {
+
+    private Servo multiRevServo;
+    private CRServo crServo;
+
+    private double position = 0;
+    private double increment = 0.00025;
+    private double power = 1;
+
+    @Override
+    protected void onInit() {
+        multiRevServo = (Servo) hardwareMap.get("BigServo");
+        crServo = (CRServo) hardwareMap.get("CrServo");
+    }
+
+    @Override
+    protected void activeLoop() throws InterruptedException {
+        if (gamepad2.dpad_down && position > 0) {
+            position -= increment;
+            multiRevServo.setPosition(position);
+            getTelemetryUtil().addData("dpad down position=", position);
+            getTelemetryUtil().sendTelemetry();
+        }
+        else if (gamepad2.dpad_up && position < 1)
+        {
+            position += increment;
+            multiRevServo.setPosition(position);
+            getTelemetryUtil().addData("dpad up position=", position);
+            getTelemetryUtil().sendTelemetry();
+        }
+
+        if (gamepad2.dpad_left) {
+            crServo.setPower(power);
+            getTelemetryUtil().addData("dpad left power=", crServo.getPower());
+            getTelemetryUtil().sendTelemetry();
+        }
+        else if (gamepad2.dpad_right) {
+            crServo.setPower(-1);
+            getTelemetryUtil().addData("dpad right power=", crServo.getPower());
+            getTelemetryUtil().sendTelemetry();
+        }
+        else {
+            crServo.setPower(0);
+        }
+    }
+}
