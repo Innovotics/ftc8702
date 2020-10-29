8e5b1c7d78d19bee66f97f1130b9c48965a4d1cc
Ben Szeto <benkszeto@gmail.com>
2020-10-29 16:38:30 -0400

    Ring detection with zoom

--- src/main/java/org/ftc8702/components/motors/MecanumWheelDriveTrain.java
+++ src/main/java/org/ftc8702/components/motors/MecanumWheelDriveTrain.java
@@ -116,6 +116,77 @@ public class MecanumWheelDriveTrain {
         backLeftMotor.setPower(0.2);
         backRightMotor.setPower(-1);
     }
+
+    private static final double TICKS_PER_INCHES = 1446 / 4.71;
+
+    public void goForwardByInches(double distanceInches, double power) {
+        double ticks =  distanceInches * TICKS_PER_INCHES;
+        goForwardWithOdometer(ticks, ticks, power);
+    }
+
+    public void goForwardWithOdometer(double leftTargetTicks, double rightTargetTicks, double power) {
+        // right - go forward will go negative
+        // left - go forward will go positive
+        // at beginning current = 0, righttarget = -1450 ==> go forward
+        // at beginning current = 0, lefttarget = 1450 ==> go forward
+
+        int frontLeftTicks = -1 * frontLeftMotor.getCurrentPosition(); // flip sign to make both sides same direction
+        int frontRightTicks = frontRightMotor.getCurrentPosition();
+        telemetry.addData("ticks", "target=" + leftTargetTicks
+                + " frontLeftTicks=" + frontLeftTicks + " frontRightTicks=" + frontRightTicks);
+        telemetry.update();
+
+        if (frontLeftTicks <= leftTargetTicks && frontRightTicks <= rightTargetTicks)
+        {
+            if (frontRightTicks > frontLeftTicks + 100) { //if turn right too much
+                telemetry.addData("Right > Left", "");
+                frontLeftMotor.setPower(-power/2);
+                frontRightMotor.setPower(power); // because motor is on the opposite side
+                backLeftMotor.setPower(power/2);
+                backRightMotor.setPower(power);
+            } else if (frontLeftTicks > frontRightTicks + 100) { // if turn left too much
+                telemetry.addData("Left > Right", "");
+                frontLeftMotor.setPower(-power);
+                frontRightMotor.setPower(power/2); // because motor is on the opposite side
+                backLeftMotor.setPower(power);
+                backRightMotor.setPower(power/2);
+            } else {
+                telemetry.addData("go stright", "");
+                goForward((float) power);
+            }
+            telemetry.update();
+        }
+        else
+        {
+            stop();
+        }
+
+        /*
+        if (leftOdometer <= frontLeftMotor.getCurrentPosition() && rightOdometer <= frontRightMotor.getCurrentPosition()) {
+
+            if (rightOdometer > leftOdometer + 30) { //if turn right too much
+                frontLeftMotor.setPower(-power);
+                frontRightMotor.setPower(power - (.1)); // because motor is on the opposite side
+                backLeftMotor.setPower(-power - (.1));
+                backRightMotor.setPower(power);
+
+            } else if (leftOdometer < rightOdometer - 30) { // if turn left too much
+                frontLeftMotor.setPower(-power + (.1));
+                frontRightMotor.setPower(power); // because motor is on the opposite side
+                backLeftMotor.setPower(-power + (.1));
+                backRightMotor.setPower(power);
+            } else{
+                frontLeftMotor.setPower(-power);
+                frontRightMotor.setPower(power);
+                backLeftMotor.setPower(-power);
+                backRightMotor.setPower(power);
+            }
+
+            sleep(500);
+        }
+        stop();
+         */
+    }
     // when power = 1
     private double speedForwardInFtPerSecond = (1.3 * 2) / 0.968;// speed = distance / time
 
--- src/main/java/org/ftc8702/opmodes/ultimategoal/OdometerAutonomous.java
+++ src/main/java/org/ftc8702/opmodes/ultimategoal/OdometerAutonomous.java
@@ -1,4 +1,69 @@
 package org.ftc8702.opmodes.ultimategoal;
 
-public class OdometerAutonomous {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.ftc8702.components.motors.MecanumWheelDriveTrain;
+import org.ftc8702.configurations.production.OdometerRobotConfiguration;
+
+import ftcbootstrap.ActiveOpMode;
+
+@Autonomous(name = "OdometerAutonomous", group = "Ops")
+public class OdometerAutonomous extends ActiveOpMode {
+
+    private OdometerRobotConfiguration driveTrainConfig;
+    private MecanumWheelDriveTrain driveTrain;
+
+    // odometer 1446 ticks = 4.7 inches (1 circumference = 1.5 inch diameter * pi = 4.7 inches)
+    int targetLeftValue = 1446;
+    int targetRightValue = 1446;
+    int targetCenterValue = 0;
+
+
+    /**
+     * Implement this method to define the code to run when the Init button is pressed on the Driver station.
+     */
+    @Override
+    public void onInit() {
+
+        driveTrainConfig = OdometerRobotConfiguration.newConfig(hardwareMap, getTelemetryUtil());
+
+        //Note The Telemetry Utility is designed to let you organize all telemetry data before sending it to
+        //the Driver station via the sendTelemetry command
+        getTelemetryUtil().addData("Init", getClass().getSimpleName() + " initialized.");
+
+    }
+
+    @Override
+    public void onStart() throws InterruptedException {
+        super.onStart();
+        driveTrain = new MecanumWheelDriveTrain(driveTrainConfig.motorFL, driveTrainConfig.motorFR, driveTrainConfig.motorBL, driveTrainConfig.motorBR, telemetry);
+
+        driveTrain.frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveTrain.frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveTrain.backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveTrain.backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        driveTrain.frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        driveTrain.frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        driveTrain.backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        driveTrain.backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        telemetry.addData("Enocoders",  "Starting at " + driveTrain.frontRightMotor.getCurrentPosition() + "," +
+                driveTrain.frontLeftMotor.getCurrentPosition() + "," + driveTrain.backRightMotor.getCurrentPosition());
+        telemetry.update();
+        //driveTrain.goForwardWithOdometer(1000,1000,0.1);
+    }
+
+    @Override
+    protected void activeLoop() throws InterruptedException {
+
+        // driveTrain.goForwardWithOdometer(targetLeftValue,targetRightValue,0.2);
+        driveTrain.goForwardByInches(24.0,0.4);
+
+        telemetry.addData("Enocoders",  "Starting at  right: " + driveTrain.frontRightMotor.getCurrentPosition() + ", left: " +
+                driveTrain.frontLeftMotor.getCurrentPosition() + ", middle: " + driveTrain.backRightMotor.getCurrentPosition() + ", difference: "+ (driveTrain.frontRightMotor.getCurrentPosition() - -driveTrain.frontLeftMotor.getCurrentPosition()));
+        telemetry.update();
+        //driveTrain.goForward((float) 0.5);
+    }
 }
